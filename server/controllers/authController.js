fa90ef8ab9f615ac1cd61afe13215f8a79677a59
Sanjib Adhya <code.gorrilla@gmail.com>
2026-04-05 00:02:04 +0530

    developed controller function for user registration

---
+++ server/controllers/authController.js
@@ -0,0 +1,37 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+//controller function for registration
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: "Missing Details." });
+  }
+
+  try {
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new userModel({ name, email, password: hashedPassword }); //user created
+    await user.save(); //stored in the db
+
+    //generate the token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+  } catch (err) {
+    res.json({ success: false, message: err.message });
+  }
+};
